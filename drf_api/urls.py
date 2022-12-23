357d3a7337224b61acf6530b8ce0e354986c0f03
Hammad <hammad2091504@hotmail.com>
2022-12-23 11:36:48 +0000

    Added JSON Web Tokens to the project and configured them

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -21,6 +21,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # built in rest framework
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path(
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+    ),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
--- profiles/views.py
+++ profiles/views.py
@@ -31,7 +31,10 @@ class ProfileList(generics.ListAPIView):
     ]
 
     filterset_fields = [
-        'owner__following__followed__profile',
+        # followers
+        'owner__following__followed__profile', 
+        # following
+        'owner__followed__owner__profile',
     ]
 
     ordering_fields = [
